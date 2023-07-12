a7cd788fb7d07f6043d9fd7006fadececb2ba093
Nevil Dsouza <nevildsouza1239@gmail.com>
2023-07-12 15:15:34 +0530

    Initialise React App and create navigation bar

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <nav>
+        <a href="#" className="blog-logo">
+          Blogify
+        </a>
+
+        <div>
+          <a href="#">Login</a>
+          <a href="#">Register</a>
+        </div>
+      </nav>
+    </main>
+  );
+}
+
+export default App;
